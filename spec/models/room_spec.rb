fcb0d56dfdae6b1e0caa8e1416493d9a05fd3225
Eliseu Ramos <eliseu.mec@gmail.com>
2023-11-02 18:39:12 -0300

    add room#valid? tests

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe '#valid?' do
+    it 'must have a name to be valid' do
+      # arrange
+      room = Room.new
+
+      # act
+      room.valid?
+
+      # assert
+      expect(room.errors.include? :name).to be true
+    end
+
+    it 'must have a description to be valid' do
+      # arrange
+      room = Room.new
+
+      # act
+      room.valid?
+
+      # assert
+      expect(room.errors.include? :description).to be true
+    end
+
+    it 'must have an area to be valid' do
+      # arrange
+      room = Room.new
+
+      # act
+      room.valid?
+
+      # assert
+      expect(room.errors.include? :area).to be true
+    end
+
+    it 'must have a maximum capacity to be valid' do
+      # arrange
+      room = Room.new
+
+      # act
+      room.valid?
+
+      # assert
+      expect(room.errors.include? :max_capacity).to be true
+    end
+
+    it 'must have a rent price to be valid' do
+      # arrange
+      room = Room.new
+
+      # act
+      room.valid?
+
+      # assert
+      expect(room.errors.include? :rent_price).to be true
+    end
+  end
 end
--- spec/system/rooms/host_creates_new_room_spec.rb
+++ spec/system/rooms/host_creates_new_room_spec.rb
@@ -20,36 +20,6 @@ describe 'User creates a new room for an inn' do
     expect(current_path).to eq new_user_session_path
   end
 
-  # it 'only for a inn which the user is the host' do 
-  #   # arrange
-  #   user = User.create!(email: 'test@gmail.com', password: 'password', 
-  #                       role: :host)
-  #   another_user = User.create!(email: 'test2@gmail.com', password: 'password',
-  #                               role: :host)
-  #   inn = user.create_inn!(brand_name: 'Pousada Teste', 
-  #                     registration_number: '58277983000198', 
-  #                     phone_number: '(11) 976834383', checkin_time: '18:00',
-  #                     checkout_time: '11:00', address_attributes: {
-  #                       street_name: 'Av. da Pousada', number: '10', 
-  #                       neighborhood: 'Bairro da Pousada', city: 'São Paulo',
-  #                       state: 'SP', zip_code: '05616-090'})
-  #   another_user.create_inn!(brand_name: 'Pousada Teste 2', 
-  #                       registration_number: '582779834230198', 
-  #                       phone_number: '(11) 564651321', checkin_time: '18:00',
-  #                       checkout_time: '11:00', address_attributes: {
-  #                         street_name: 'Av. da Pousada', number: '10', 
-  #                         neighborhood: 'Bairro da Pousada', city: 'São Paulo',
-  #                         state: 'SP', zip_code: '05616-090'})
-
-  #   # act
-  #   login_as another_user
-  #   visit new_inn_room_path(inn.id)
-
-  #   # assert
-  #   expect(page).to have_content 'Você não possui autorização para essa ação.'
-  #   expect(current_path).to eq root_path
-  # end
-
   it 'starting from the homepage' do
     # arrange
     user = User.create!(email: 'test@gmail.com', password: 'password', 
